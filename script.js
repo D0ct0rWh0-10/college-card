568a4686212d32e89c15bfa16d8d91d6cb02ad27
D0ct0rWh0-10 <65511878+D0ct0rWh0-10@users.noreply.github.com>
2020-05-18 14:27:57 +0300

    College-card
    MosGU

---
+++ script.js
@@ -0,0 +1,14 @@
+
+
+$(document).ready(function(){
+    $('#button').on("click", function(){  
+        $('.qr').toggle();
+                $('.qrtwo').toggle(); 
+    });
+});
+
+  if (randomElement.text.length > 40) {
+    advice.style.fontSize = '33px';
+  } else {
+    advice.style.fontSize = '42px';
+  }
